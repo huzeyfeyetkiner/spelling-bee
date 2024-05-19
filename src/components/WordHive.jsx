2daa32250a32b4d5a206ab5b5fa9ae5cc89175dd
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2024-05-19 17:23:39 +0300

    WordHive style adjustments

--- src/components/WordHive.jsx
+++ src/components/WordHive.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function WordHive({ char }) {
 	return (
-		<div className="w-12 h-12 flex justify-center items-center rounded-xl bg-[#272827] text-color-text">
+		<div className="w-12 h-12 flex justify-center items-center rounded-xl bg-[#272827] text-color-text border border-color-text">
 			{char}
 		</div>
 	)
