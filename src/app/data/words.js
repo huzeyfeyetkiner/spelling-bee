778c80116b7c5fb085cb8a5102575990c3ba9099
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2024-05-19 17:23:26 +0300

    words data created

--- src/app/data/words.js
+++ src/app/data/words.js
@@ -16,6 +16,66 @@ export const words = {
 			word: "elma",
 			hive: "elm-",
 		},
+		{
+			word: "çilek",
+			hive: "ç-l-k",
+		},
+		{
+			word: "portakal",
+			hive: "p-rt-k-l",
+		},
+		{
+			word: "armut",
+			hive: "r-m-t",
+		},
+		{
+			word: "üzüm",
+			hive: "z-m",
+		},
+		{
+			word: "kiraz",
+			hive: "k-r-z",
+		},
+		{
+			word: "muz",
+			hive: "m-z",
+		},
+		{
+			word: "mandalina",
+			hive: "m-nd-l-n-",
+		},
+		{
+			word: "nar",
+			hive: "n-r",
+		},
+		{
+			word: "kavun",
+			hive: "k-v-n",
+		},
+		{
+			word: "incir",
+			hive: "n-c-r",
+		},
+		{
+			word: "greyfurt",
+			hive: "gr-y-f-rt",
+		},
+		{
+			word: "avokado",
+			hive: "v-k-d-",
+		},
+		{
+			word: "kivi",
+			hive: "k-v-",
+		},
+		{
+			word: "ananas",
+			hive: "n-n-s",
+		},
+		{
+			word: "dut",
+			hive: "d-t",
+		},
 	],
 	en: [
 		{
@@ -28,15 +88,71 @@ export const words = {
 		},
 		{
 			word: "apricot",
-			hive: "p-r-c-t",
+			hive: "-p-r-c-t",
 		},
 		{
 			word: "apple",
-			hive: "p-l-",
+			hive: "-p-l-",
 		},
 		{
 			word: "banana",
 			hive: "b-n-n-",
+		},
+		{
+			word: "strawberry",
+			hive: "s-t-rw-b-rr-",
+		},
+		{
+			word: "orange",
+			hive: "-r-ng-",
+		},
+		{
+			word: "pear",
+			hive: "p--r",
+		},
+		{
+			word: "grape",
+			hive: "g-r-p-",
+		},
+		{
+			word: "cherry",
+			hive: "ch-rr-",
+		},
+		{
+			word: "melon",
+			hive: "m-l-n",
+		},
+		{
+			word: "mandarin",
+			hive: "m-nd-r-n",
+		},
+		{
+			word: "pomegranate",
+			hive: "p-m-gr-n-t-",
+		},
+		{
+			word: "fig",
+			hive: "f-g",
+		},
+		{
+			word: "grapefruit",
+			hive: "gr-p-fr--t",
+		},
+		{
+			word: "avocado",
+			hive: "-v-c-d-",
+		},
+		{
+			word: "kiwi",
+			hive: "k-w-",
+		},
+		{
+			word: "pineapple",
+			hive: "p-n--ppl-",
+		},
+		{
+			word: "mulberry",
+			hive: "m-lb-rr-",
 		},
 	],
 }
